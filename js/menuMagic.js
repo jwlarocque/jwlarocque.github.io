98df00e2c9b7ba7314e465e20ff91296cf228ae9
jwlarocque <jwlarocque@gmail.com>
2020-04-24 16:11:35 -0600

    Keep the carousel in place on reload

--- js/menuMagic.js
+++ js/menuMagic.js
@@ -21,17 +21,27 @@ function initMenu(){
     });
 
     pane1button = document.querySelector("#pane1button")
-    pane1button.addEventListener("click", event => {
-        document.querySelector(".carousel").setAttribute("style", "transform: translate(0)");
-        pane1button.setAttribute("class", "active");
-        pane2button.setAttribute("class", "")
-    });
+    pane1button.addEventListener("click", unshiftPane);
     pane2button = document.querySelector("#pane2button")
-    pane2button.addEventListener("click", event => {
-        document.querySelector(".carousel").setAttribute("style", "transform: translate(-50%)");
-        pane1button.setAttribute("class", "");
-        pane2button.setAttribute("class", "active");
-    });
+    pane2button.addEventListener("click", shiftPane);
+
+    if (window.location.hash === "#projects") {
+        shiftPane();
+    }
+}
+
+function unshiftPane(event) {
+    window.location = "";
+    document.querySelector(".carousel").setAttribute("style", "transform: translate(0)");
+    pane1button.setAttribute("class", "active");
+    pane2button.setAttribute("class", "")
+}
+
+function shiftPane(event) {
+    window.location = "#projects";
+    document.querySelector(".carousel").setAttribute("style", "transform: translate(-50%)");
+    pane1button.setAttribute("class", "");
+    pane2button.setAttribute("class", "active");
 }
 
 
