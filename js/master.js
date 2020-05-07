e092b892389b986eac82a02f9e1d109bd2ffc496
jwlarocque <jwlarocque@gmail.com>
2020-05-07 14:46:43 -0600

    Update a few things

--- js/master.js
+++ js/master.js
@@ -3,7 +3,8 @@ footerStrings = [
     "<a href=\"https://en.wikipedia.org/wiki/Collier_(ship)#Coals_from_Newcastle\" style=\"text-decoration:none; color:inherit\">Wikipedia - Collier (ship)</a>",
     "<a href=\"https://www.goodreads.com/book/show/23444482-the-traitor-baru-cormorant\" style=\"text-decoration:none; color:inherit\">The Traitor Baru Cormorant (novel)</a>", // Sorry for the Goodreads link, but I think it's more useful than the author's website.
     "<a href=\"http://www.charts.noaa.gov/OnLineViewer/17382.shtml\" style=\"text-decoration:none; color:inherit\">NOAA chart 17382 (Zarembo Island)</a>",
-    "<a href=\"https://www.youtube.com/channel/UCworsKCR-Sx6R6-BnIjS2MA\" style=\"text-decoration:none; color:inherit\">Clickspring (Clockmaking videos)</a>"
+    "<a href=\"https://www.youtube.com/channel/UCworsKCR-Sx6R6-BnIjS2MA\" style=\"text-decoration:none; color:inherit\">Clickspring (Clockmaking videos)</a>",
+    "<a href=\"https://99percentinvisible.org/episode/the-natural-experiment/\" style=\"text-decoration:none; color:inherit\">99% Invisible: The Natural Experiment</a>"
 ]
 
 function fillFooterText() {
